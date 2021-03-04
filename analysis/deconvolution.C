1626558453c6d2cfba1d935451772f4868adff1a
Zhongling Ji <jizhongling@gmail.com>
2021-03-04 11:06:30 -0500

    Use analysis/shieldingAna.C

--- analysis/deconvolution.C
+++ analysis/deconvolution.C
@@ -1,9 +1,9 @@
 // this does 3W region analysis for epInelastic
 // [process][ring][sector]
-const int nMatrix=2;
+const int nMatrix=6;
 const int nProcDef=10;
-const int nProc=2;
-double A[nProc][6][3],rate[nProc][6][3],sigma[nProc][6][3];
+const int nProc=9;
+double A[nProcDef][6][3],rate[nProc][6][3];
 string procNm[nProcDef]={
   "moller",
   "epElastic",
@@ -16,10 +16,6 @@ string procNm[nProcDef]={
   "piminus",
   "neutralBknd"
 };
-string procNmComb[nProc]={
-  "moller",
-  "Bknd"
-};
 
 int verbose = 0;
 
@@ -28,7 +24,7 @@ const double neutralBkgndRate[6]={1e9,1e9,1e9,1e9,1e9,1e9};
 //const double neutralBkgndRate[6]={5e7,8e7,11e7,7e7,33e7,3e7};
 
 void analyzeOne(int ring, int sect);
-void readSim(string fnm,int proc,int addProc);
+void readSim(string fnm,int proc, int addBkgnd);
 void printAll();
 
 void deconvolution(){
@@ -38,38 +34,41 @@ void deconvolution(){
       for(int k=0;k<3;k++){
         A[i][j][k]=0;
         rate[i][j][k]=0;
-        sigma[i][j][k]=0;
       }
 
   /// optimal positioning
   string fnms[nProcDef]={
-    "histos/deconv_ee_offSet0_basicAnaV0.root",
-    "histos/deconv_epE_offSet0_basicAnaV0.root",
-    "histos/inelasticout_bkgAnaV4.root",
-    "histos/inelasticout_bkgAnaV4.root",
-    "histos/inelasticout_bkgAnaV4.root",
-    "histos/deconv_eAlE_offSet0_basicAnaV0.root",
-    "histos/deconv_eAlQ_offSet0_basicAnaV0.root",
-    "histos/deconv_eAlI_offSet0_basicAnaV0.root",
-    "histos/deconv_pi_offSet0_basicAnaV0.root",
-    "byHand"
+		      // "../ee_Segmented_shldAnaV6.root",
+		      // "../epElastic_Segmented_shldAnaV6.root",
+		      // "../epInelastic_Segmented_shldAnaV7.root",
+		      // "../epInelastic_Segmented_shldAnaV7.root",
+		      // "../epInelastic_Segmented_shldAnaV7.root",
+		      // "../ee_SegmentedV1_shldAnaV7.root",
+		      // "../epElastic_SegmentedV1_shldAnaV7.root",
+		      // "../epInelastic_SegmentedV1_shldAnaV7.root",
+		      // "../epInelastic_SegmentedV1_shldAnaV7.root",
+		      // "../epInelastic_SegmentedV1_shldAnaV7.root",
+		      "histos/ee_Hybrid_shldAnaV7.root",
+		      "histos/epElastic_Hybrid_shldAnaV7.root",
+		      "histos/epInelastic_Hybrid_shldAnaV7.root",
+		      "histos/epInelastic_Hybrid_shldAnaV7.root",
+		      "histos/epInelastic_Hybrid_shldAnaV7.root",
+		      "histos/eAlElastic_bkgAnaV4.root",
+		      "histos/eAlQuasielastic_bkgAnaV4.root",
+		      "histos/eAlInelastic_bkgAnaV4.root",
+		      "histos/deconv_pi_offSet0_basicAnaV0.root",
+		      "byHand"
   };
 
-  int addProc(0);
-  for(int i=0;i<nProc;i++){
-    readSim(fnms[i+addProc],i,addProc);
-    if(i==1)
-      while(addProc<8){
-        addProc++;
-        readSim(fnms[i+addProc],i,addProc);
-      }
-  }
-
-  for(int i=0;i<6;i++)
-    for(int j=0;j<3;j++)
-      analyzeOne(i,j);
+  for(int i=0;i<nProc;i++)
+    readSim(fnms[i],i,0);
 
   printAll();
+  
+  // for(int i=0;i<6;i++)
+  //   for(int j=0;j<3;j++)
+  //     analyzeOne(i,j);
+  analyzeOne(4,2);
 }
 
 void analyzeOne(int ring, int sect){
@@ -83,7 +82,7 @@ void analyzeOne(int ring, int sect){
   if(verbose){
     cout<<"\tAsym and rate\n";
     for(int i=0;i<nProc;i++)
-      cout<<"\t\t"<<procNmComb[i]<<"\t"<<A[i][ring][sect]<<"\t"<<rate[i][ring][sect]<<endl;
+      cout<<"\t\t"<<procNm[i]<<"\t"<<A[i][ring][sect]<<"\t"<<rate[i][ring][sect]<<endl;
   }
 
   const double polarization = 0.8;
@@ -151,13 +150,14 @@ void analyzeOne(int ring, int sect){
     double asym(0);
     for(int j=0;j<nMatrix;j++)
       asym += F(i,j) * B[j];
-    cout<<"Asymmetry "<<procNmComb[i]<<"\t"<<asym<<endl;
+    cout<<"Asymmetry "<<procNm[i]<<"\t"<<asym<<endl;
   }
 
   cout<<endl<<endl<<"\t\t\toverall\nName\tAsymmetry\tuncert[ppb]\trelative uncer[%]\n";
+  double sigma[nProcDef];
   for(int i=0;i<nMatrix;i++){
-    sigma[i][ring][sect] = sqrt( F(i,i) ) / ( 0.8 * sqrt(beamDays * days2seconds) ) * 1e9;
-    cout<<procNmComb[i]<<"\t"<<A[i][ring][sect]<<"\t"<<sigma[i][ring][sect]<<"\t"<<sigma[i][ring][sect]/fabs(A[i][ring][sect])*100.<<endl;
+    sigma[i] = sqrt( F(i,i) ) / ( 0.8 * sqrt(beamDays * days2seconds) ) * 1e9;
+    cout<<procNm[i]<<"\t"<<A[i][ring][sect]<<"\t"<<sigma[i]<<"\t"<<sigma[i]/fabs(A[i][ring][sect])*100.<<endl;
   }
 
   cout<<endl<<"\t\t\tin the selected bin\n";
@@ -168,14 +168,14 @@ void analyzeOne(int ring, int sect){
     ( sqrt(totRate) * polarization * sqrt(beamDays * days2seconds)) *1e9;
   cout<<"stat moller\t"<< stat << "\t" << stat/33<<endl;
 
-  double syst[nProc];
-  double uncert[nProc]={};
+  double syst[nProcDef];
+  double uncert[nProcDef]={0,0,0,0,1,1,0.1,1};
   for(int i=1;i<nProc;i++){
     if(i<nMatrix)
-      syst[i] = rate[i][ring][sect]/rate[0][ring][sect] * sigma[i][ring][sect];
+      syst[i] = rate[i][ring][sect]/rate[0][ring][sect] * sigma[i];
     else
       syst[i] = rate[i][ring][sect]/rate[0][ring][sect] * fabs(A[i][ring][sect]) * uncert[i] ;
-    cout<<"syst "<<procNmComb[i]<<"\t"<<syst[i]<<"\t"<<syst[i]/33<<endl;
+    cout<<"syst "<<procNm[i]<<"\t"<<syst[i]<<"\t"<<syst[i]/33<<endl;
   }
 
 }
@@ -185,87 +185,79 @@ void printAll(){
   const double polarization = 0.8;
   const double beamDays = 235 + 95 + 14;
   const double days2seconds = 24*60*60;
-  const char *sector[3] = {"C", "T", "O"};
-
-  cout<<endl<<",";
-  for(int k=0;k<nProc;k++)
-    cout<<","<<procNmComb[k]<<",,,,";
-  cout<<",,,"<<endl;
-
-  cout<<"R,S";
-  for(int k=0;k<nProc;k++)
-    cout<<",A [ppb],f [% of rate],f*A [% of Am],dA [ppb],dA/A [% of A]";
-  cout<<",Am [ppb],dAm [ppb],dAm/Am [%]"<<endl;
 
   for(int i=0;i<6;i++)
     for(int j=0;j<3;j++){
-      double rateTot(0), fAsum(0);
-      for(int k=0;k<nProc;k++){
+      double rateTot(0);
+      for(int k=0;k<nProc;k++)
         rateTot += rate[k][i][j];
-        fAsum += rate[k][i][j] * A[k][i][j];
-      }
       double sigmaAm = 1/sqrt(rateTot);
 
-      double Ami2(0);
-      cout<<i+1<<","<<sector[j];
+      double Ami2(0),f2[nProcDef];
+      cout<<i<<" "<<j;
       for(int k=0;k<nProc;k++){
         Ami2 += rate[k][i][j]/rateTot * A[k][i][j];
-        cout<<","<<A[k][i][j]<<","<<rate[k][i][j]/rateTot*100.<<","<<rate[k][i][j]*A[k][i][j]/fAsum*100.
-          <<","<<sigma[k][i][j]<<","<<(fabs(A[k][i][j])>1e-9?sigma[k][i][j]/fabs(A[k][i][j])*100.:0);
+	cout<<" "<<A[k][i][j]<<" "<<rate[k][i][j]/rateTot;
       }
-      double dAmi2 = sigmaAm / ( 0.8 * sqrt(beamDays * days2seconds) ) * 1e9;
-      cout<<","<<Ami2<<","<<dAmi2<<","<<dAmi2/fabs(Ami2)*100.<<endl;
+      cout<<" "<<Ami2<<" "<<sigmaAm / ( 0.8 * sqrt(beamDays * days2seconds) ) * 1e9<< " " << rateTot <<endl;
 
     }
 }
 
-void readSim(string fnm,int proc,int addProc){
+void readSim(string fnm,int proc, int addBkgnd){
 
-  if(procNm[proc+addProc] == "neutralBknd"){
-    for(int i=0;i<6;i++)
-      for(int j=0;j<3;j++){
-        double thisA = 0;
-        double thisRate = neutralBkgndRate[i]/3*neutralBkgndFactor;
-        A[proc][i][j] = (rate[proc][i][j]*A[proc][i][j] + thisRate*thisA) / (rate[proc][i][j] + thisRate);
-        rate[proc][i][j] += thisRate;
+  if(proc>=9){
+  for(int i=0;i<6;i++)
+    for(int j=0;j<3;j++)
+      if(proc==7){
+	rate[proc][i][j] = neutralBkgndRate[i]/3*neutralBkgndFactor;
+	A[proc][i][j] = 0;
+      }else{
+	rate[proc][i][j] = 0;
+	A[proc][i][j] = 0;
       }
     return;
   }
   
-  size_t inel = procNm[proc+addProc].find("epInelasticW");
-  size_t Wbin = procNm[proc+addProc].size();
-  if(verbose) cout<<"reading "<<fnm<<"\t"<<proc+addProc<<endl;
+  if(verbose) cout<<"reading "<<fnm<<"\t"<<proc<<endl;
   TFile *fin=TFile::Open(fnm.c_str(),"READ");
   string hName="hRate";
-  if(inel != string::npos)
-    hName = Form("hRate_%s",procNm[proc+addProc].substr(Wbin-2,2).c_str());
+  if(proc<2)
+    hName = "det28/hRate_eP1";
+  else if(proc>=2 && proc<5)
+    hName = Form("deconvolution/hRateW%d_eP1",proc-1);
 
   TH1D *hRate=(TH1D*)fin->Get(hName.c_str());
 
   const double rateFactor = 65./85;
   double gfFactor = 1;
+  //if(proc >=5 && proc <= 6)
+  //  gfFactor = 1e6;
+  if(proc<5)
+    gfFactor *= -1;
+  //double minRate(1e9);
   for(int i=0;i<6;i++)
     for(int j=0;j<3;j++){
       hName=Form("hAsym_R%d_S%d",i+1,j);
-      if(inel != string::npos)
-        hName=Form("hAsym_%s_R%d_S%d",procNm[proc+addProc].substr(Wbin-2,2).c_str(),i+1,j);
+      if(proc<2)
+	hName=Form("deconvolution/hAsym_eP1_R%d_S%d",i+1,j);
+      else if(proc>=2 && proc <5) 
+	hName=Form("deconvolution/hAsym_W%d_eP1_R%d_S%d",proc-1,i+1,j);
 
       if(verbose)
 	cout<<"\tR/S\t"<<i<<"\t"<<j<<"\t"<<hName<<endl;
 
-      double thisA;
       TH1D *hA=(TH1D*)fin->Get(hName.c_str());
-      if(procNm[proc+addProc] == "piminus")
-        thisA = -1660;
+      if(proc==8)
+	A[proc][i][j] = -1660;
       else
-        thisA = hA->GetMean()*gfFactor;
+	A[proc][i][j] = hA->GetMean()*gfFactor;
       
-      double thisRate = hRate->GetBinContent(i*3+j+1) * rateFactor;
+      rate[proc][i][j] = hRate->GetBinContent(i*3+j+1) * rateFactor;
+      // if(rate[proc][i][j]<min && rate[proc][i][j]>0)
+      // 	minRate=rate[proc][i][j];
       if(verbose)
-	cout<<"\tR/S\t"<<i<<"\t"<<j<<"\t"<<thisA<<"\t"<<thisRate<<endl;
-      
-      A[proc][i][j] = (rate[proc][i][j]*A[proc][i][j] + thisRate*thisA) / (rate[proc][i][j] + thisRate);
-      rate[proc][i][j] += thisRate;
+	cout<<"\tR/S\t"<<i<<"\t"<<j<<"\t"<<A[proc][i][j]<<"\t"<<rate[proc][i][j]<<endl;
     }
 
   fin->Close();
